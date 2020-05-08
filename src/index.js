b820a4b311ae22affbd0b567f7a5ff743aae5754
Naomi Calabretta <me@arytonex.pw>
2020-05-08 16:31:47 +0200

    Some work

--- src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const prompt = require('prompt-sync')({sigint: true});
-console.log('Glasscordify\n');
-console.log('Please specify what you want to patch');
-console.log('Discord is NOT supported');
-const app = prompt('App: ');
-console.log(app);
