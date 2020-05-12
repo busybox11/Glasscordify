8628f2b4f92db119adaa721670090bc3205f813f
Naomi Calabretta <me@arytonex.pw>
2020-05-12 21:28:12 +0200

    initial gui routines

--- gui/app/lib_interface.js
+++ gui/app/lib_interface.js
@@ -15,6 +15,7 @@
 */
 "use strict";
 
+const path = require("path");
 const lib = require("glasscordify-lib");
 
 module.exports = class LibInterface {
@@ -49,6 +50,76 @@ module.exports = class LibInterface {
 	
 	static uninstallClicked(win){
 		console.log("uninstall clicked");
+	}
+	
+	static dropFilter(files){
+		return files.filter((x) => {
+			return ["",".exe",".desktop",".app",".lnk"].includes(path.extname(x))
+		});
+	}
+	
+	static async install(win, executableFile){
+		win.webContents.send("install-checking-app");
+		// Resolve the executable path
+		executableFile = await lib.Utils.getAbsoluteExecutableFile(executableFile);
+		if(typeof executableFile === "undefined"){
+			win.webContents.send("error", "Given file is not valid!");
+			return false;
+		}
+		const app = lib.Installer.checkApp(executableFile);
+		if(app.error){
+			win.webContents.send("error", app.error);
+			return false;
+		}
+		
+		const result = lib.Utils.install(app);
+		if(result.error){
+			win.webContents.send("error", result.error);
+			return false;
+		}
+		
+		if(app.appPathType === "asar"){ // we worked in our temp dir!
+			lib.Utils.moveAsarToBackupFolder(app.resourcesPath);
+			lib.Utils.commitAsarExtraction(app.resourcesPath);
+		}
+		
+		win.webContents.send("install-success");
+		return true;
+	}
+	
+	static async uninstall(win, executableFile){
+		win.webContents.send("uninstall-checking-app");
+		// Resolve the executable path
+		executableFile = await lib.Utils.getAbsoluteExecutableFile(executableFile);
+		if(typeof executableFile === "undefined"){
+			win.webContents.send("error", "Given file is not valid!");
+			return false;
+		}
+		const app = lib.Installer.checkApp(executableFile);
+		if(app.error){
+			win.webContents.send("error", app.error);
+			return false;
+		}
+		
+		if(app.appPathType === "asar:patched"){ // we can easily "unpatch" this
+			if(!lib.Utils.revertAsarPatching(app.resourcesPath)){
+				win.webContents.send("error", "Couldn't revert changes to the app!");
+				return false;
+			}
+			win.webContents.send("uninstall-success");
+			return true;
+		}
+		
+		// other patch types
+		
+		const result = lib.Utils.uninstall(app);
+		if(result.error){
+			win.webContents.send("error", result.error);
+			return false;
+		}
+		
+		win.webContents.send("uninstall-success");
+		return true;
 	}
 	
 }
